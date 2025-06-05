a1f5db1bbcff4600b55e7e8a2cc5356aa74c72c4
MillaMirjami <milla.sukki@gmail.com>
2025-06-05 16:06:59 +0300

    token logic added to front end, works

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Notification from './components/Notification'
 import Footer from './components/Footer'
 import noteService from './services/notes'
 import loginService from './services/login'
+import LoginForm from "./components/LoginForm"
+import NoteForm from "./components/NoteForm"
 
 const App = () => {
-  const [notes, setNotes] = useState(null)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true) 
-  const [errorMessage, setErrorMessage] = useState('Error messages are shown here')
+  const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
@@ -30,8 +32,9 @@ const App = () => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      // important: Math.random() > 0.5,
+      important: Math.random() > 0.5,
     }
+
     noteService
     .create(noteObject)
     .then(returnedNote => {
@@ -67,17 +70,15 @@ const App = () => {
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
-
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
-
-
+  
   const handleLogin = async (event) => {
     event.preventDefault()
-
+    
     try {
       const user = await loginService.login({
         username, password,
       })
+      noteService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -87,26 +88,22 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
-
+    
     console.log('logging in with', username, password)
   }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage}/>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input type="text" value={username} name="Username" onChange={({target}) => setUsername(target.value)}
-          />
+      {!user && <LoginForm handleLogin={handleLogin} username={username} password={password} setUsername={setUsername} setPassword={setPassword}/>}
+      {user && <div>
+        <p>{user.name} logged in</p>
+          <NoteForm addNote={addNote} newNote={newNote}  handleNoteChange={handleNoteChange}/>
         </div>
-        <div>
-          password
-          <input type="password" value={password} name="Password" onChange={({target}) => setPassword(target.value)}/>
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      }
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -121,10 +118,6 @@ const App = () => {
           />
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
-        <button type="submit">save</button>
-      </form>
       <Footer/>
     </div>
   )
